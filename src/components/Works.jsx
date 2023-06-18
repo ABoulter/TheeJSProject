d1ef53cd1654c77a37a247022cceaba446889359
Exo <paink37@hotmail.com>
2023-06-18 14:56:38 +0100

    ss

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Tilt } from "react-tilt";
 import { motion, AnimatePresence } from "framer-motion";
 import { styles } from "../styles";
-import { github } from "/assets";
+import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
